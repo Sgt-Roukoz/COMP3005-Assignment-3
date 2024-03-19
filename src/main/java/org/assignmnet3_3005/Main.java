b867184c8d1851702ee551b03606f270b5a38953
Marwan Zeid <45441599+Sgt-Roukoz@users.noreply.github.com>
2024-03-18 22:21:49 -0400

    Video Demonstration

--- src/main/java/org/assignmnet3_3005/Main.java
+++ src/main/java/org/assignmnet3_3005/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         DatabaseInteraction db = new DatabaseInteraction();
 
+        //db.addStudent("Marwan", "Zeid", "marwan.zeid@cmail.carleton.ca", "2021-09-02");
+        //db.updateStudentEmail(3, "jimbean@example.ca");
+        //db.deleteStudent(3);
         db.getAllStudents();
         db.closeConnection();
     }
